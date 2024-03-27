e0dd71e2fea1125fce195bf5fa430d26a00daa89
Proxy <92277920+SevenProxy@users.noreply.github.com>
2024-03-26 22:54:49 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,16 @@ import Main from "./components/main/Main.vue";
 //<HelloWorld msg="Vite + Vue" />
 const user: any = ref();
 const ID: string = "1083206341935497296";
-const x: string = "MTIyMjM0ODc2NzExNDgyNTgwMg.GTHYEW." + "uA5EZk6gyjbwkDbQw6397cSuHU3YYeixwxFeqk"
+const x: string = "MTIyMjM0ODc2NzExNDgyNTgwMg.GTHYEW." + "uA5EZk6gyjbwkDbQw6397cSuHU3YYeixwxFeqk";
 const isLoadingBanner: string = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fc9%2F4c%2Fe7%2Fc94ce78d80c07480d25f7acafddc15d8.gif&f=1&nofb=1&ipt=fe635b37f7cb97d686dbec10cd5a530530d4b09c15b72e934f2697241d6f7957&ipo=images";
 
 async function requestAPIUser(): Promise<void> {
   const headers = {
     "Authorization": `Bot ${x}`
   };
+  console.log(x)
   const responseAPI: AxiosResponse<any> = await axios.get(`https://discord.com/api/v9/users/1212113093828804658`, {headers});
-
+  console.log(responseAPI)
   if(responseAPI.status !== 200) return;
   user.value = responseAPI.data;
   //await axios.get("https://getip.creitomafioso.repl.co/getip");
