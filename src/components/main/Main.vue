daa3cf84f7e5858bbb5a23625f1d182744865251
Proxy <92277920+SevenProxy@users.noreply.github.com>
2024-03-26 21:47:41 -0300

    Update Main.vue

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -10,12 +10,10 @@ const emojiAboutMe: string = "https://cdn.discordapp.com/emojis/1173030335710232
                     <p class="font-2xl font-bold text-neutral-400">ABOUT ME</p>
                     <p class="about text-[#6e6f73] mt-2 text-sm">
                         <strong class="text-white text-xl">&lt;</strong>
-                        <span class="bg-[#393939] px-1 py-1 rounded-md text-white font-2xl">Programador</span>
+                        <span class="bg-[#393939] px-1 py-1 rounded-md text-white font-2xl">DevOps</span>
                         <strong class="text-white text-xl">></strong><br/>
-                        NOT GOD PLEASED EVERYONE🐒<br/>
-                        <span class="flex items-center">
-                            <a href="https://discord.gg/dMjYUpBKsA" class="text-sm text-blue-600">https://discord.gg/dMjYUpBKsA</a>
-                        </span>
+                        <p>#!/bin/bash</p><br/>
+                        <span class="bg-[#393939] px-1 py-1 rounded-md text-white font-2xl">Lonely, Mayumi, Laxus, Silver, Debra, Imper, Nara, Gen, Caio, Caraio and Simplez</span><br/>
                     </p>
                     <p class="text-md mt-8 font-bold text-neutral-400">DISCORD MEMBER SINCE</p>
                     <p class="text-[#6e6f73] font-xl">Jul 20, 2017</p>
@@ -23,8 +21,8 @@ const emojiAboutMe: string = "https://cdn.discordapp.com/emojis/1173030335710232
                 <div class="border-t-2 py-4 border-[#3a3c41] w-full flex flex-wrap items-center justify-center">
                     <div class="mr-4 cursor-pointer px-4 py-2 rounded-md border border-[#3a3c41] flex items-center justify-between ">
                         <div class="flex items-center">
-                            <img class="w-6 h-6 mr-2" src="https://www.freepngimg.com/download/logo/62372-computer-neon-instagram-icons-hd-image-free-png.png" />
-                            <p class="font-sm font-bold text-white">Forever</p>
+                            <img class="w-6 h-6 mr-2" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F09%2FGitHub_logo.png&f=1&nofb=1&ipt=7aad2e46ed25f8d0c3168909d3d8c546026fe1534303073c06a0f134af962826&ipo=images" />
+                            <p class="font-sm font-bold text-white">Seven Proxy</p>
                             <div class="ml-2 relative w-4 h-4 flex items-center justify-center">
                                 <svg aria-label="Verified Connection" class="flowerStar_a9981e absolute top-0 left-0" aria-hidden="false" role="img" width="16" height="16" viewBox="0 0 16 15.2"><path fill="hsla(228, 6%, 32.5%, 1)" fill-rule="evenodd" d="m16 7.6c0 .79-1.28 1.38-1.52 2.09s.44 2 0 2.59-1.84.35-2.46.8-.79 1.84-1.54 2.09-1.67-.8-2.47-.8-1.75 1-2.47.8-.92-1.64-1.54-2.09-2-.18-2.46-.8.23-1.84 0-2.59-1.54-1.3-1.54-2.09 1.28-1.38 1.52-2.09-.44-2 0-2.59 1.85-.35 2.48-.8.78-1.84 1.53-2.12 1.67.83 2.47.83 1.75-1 2.47-.8.91 1.64 1.53 2.09 2 .18 2.46.8-.23 1.84 0 2.59 1.54 1.3 1.54 2.09z"></path></svg>
                                 <svg class="absolute top-0 left-0" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 16 15.2"><path d="M7.4,11.17,4,8.62,5,7.26l2,1.53L10.64,4l1.36,1Z" fill="#ffffff"></path></svg>
