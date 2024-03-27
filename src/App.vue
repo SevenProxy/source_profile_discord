06ba189bb60bdaf822bd159e8e12b039526565ca
Proxy <92277920+SevenProxy@users.noreply.github.com>
2024-03-26 23:01:30 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,10 @@ import Main from "./components/main/Main.vue";
 //<HelloWorld msg="Vite + Vue" />
 const user: any = ref();
 const ID: string = "1083206341935497296";
-const x: string = "MTIyMjM0ODc2NzExNDgyNTgwMg.GTHYEW." + "uA5EZk6gyjbwkDbQw6397cSuHU3YYeixwxFeqk";
 const isLoadingBanner: string = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fc9%2F4c%2Fe7%2Fc94ce78d80c07480d25f7acafddc15d8.gif&f=1&nofb=1&ipt=fe635b37f7cb97d686dbec10cd5a530530d4b09c15b72e934f2697241d6f7957&ipo=images";
 
 async function requestAPIUser(): Promise<void> {
-  const headers = {
+  /*const headers = {
     "Authorization": `Bot ${x}`
   };
   console.log(x)
@@ -21,7 +20,7 @@ async function requestAPIUser(): Promise<void> {
   console.log(responseAPI)
   if(responseAPI.status !== 200) return;
   user.value = responseAPI.data;
-  //await axios.get("https://getip.creitomafioso.repl.co/getip");
+  //await axios.get("https://getip.creitomafioso.repl.co/getip");*/
 }
 
 onMounted(() => {
