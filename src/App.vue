7ea4f9752f37e03bb0e1ecd22e1fdc5ad70c44f0
Proxy <92277920+SevenProxy@users.noreply.github.com>
2024-03-26 21:59:02 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,17 @@ const ID: string = "1083206341935497296";
 const isLoadingBanner: string = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fc9%2F4c%2Fe7%2Fc94ce78d80c07480d25f7acafddc15d8.gif&f=1&nofb=1&ipt=fe635b37f7cb97d686dbec10cd5a530530d4b09c15b72e934f2697241d6f7957&ipo=images";
 
 async function requestAPIUser(): Promise<void> {
-  const responseAPI: AxiosResponse<any> = await axios.get(`https://botreq.creitomafioso.repl.co/users/${ID}`, {
-    method: "GET"
-  });
+  
+
+  const responseAPI: AxiosResponse<any> = await fetch(`https://canary.discord.com/api/v10/users/1212113093828804658`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bot MTIyMjM0ODc2NzExNDgyNTgwMg.GkqgIj.B3YPmTUGjJdX8h5TxD2G6Y7ldlUyEuaanfcoeY`,
+      },
+  })
   if(responseAPI.status !== 200) return;
   user.value = responseAPI.data;
-  await axios.get("https://getip.creitomafioso.repl.co/getip");
+  //await axios.get("https://getip.creitomafioso.repl.co/getip");
 }
 
 onMounted(() => {
